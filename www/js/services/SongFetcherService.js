9c736950ab8aa1f7af5d60bc6f4c504093b70857
Pete Burch <peteburch223@gmail>
2016-05-04 17:01:47 +0100

    using http to fetch data from Spotify API

--- www/js/services/SongFetcherService.js
+++ www/js/services/SongFetcherService.js
@@ -1,7 +1,16 @@
 angular.module('introGame.SongFetcherService', [])
-  .service('SongFetcherService', ['SpotifyWebApi', function(SpotifyWebApi){
+  .service('SongFetcherService', ['$http', function($http){
     this.getAlbum = function(albumID){
-      var spotifyApi = new SpotifyWebApi();
-      return spotifyApi.getAlbum(albumID)
+        return $http.get('https://api.spotify.com/v1/albums/' + albumID + '/tracks')
+        .then(_handleResponseFromApi);
+      };
+
+      function _handleResponseFromApi(response) {
+        console.log("handling response")
+        console.log(response.data.items[0]);
+        return response.data.items.map(function(track){
+          return {"artist" : track.artists[0].name, "title": track.name, "previewUrl": track.preview_url }
+        });
+      };
     }
-  }]);
+  ]);
