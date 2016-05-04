5c27f5671adfc70857fbd6559031750a68cca596
Chris Parker <cjp214@gmail.com>
2016-05-04 17:46:01 +0100

    spiked display of songs retrieved from the API

--- www/js/controllers/IntroGameController.js
+++ www/js/controllers/IntroGameController.js
@@ -1,6 +1,21 @@
 angular.module('introGame.controller',[])
-  .controller('IntroGameController', IntroGameController);
+  .controller('IntroGameController', ['SongFetcherService', function(SongFetcherService) {
 
-  function IntroGameController() {
-    this.introGame = "IntroGame1";
-  };
+    var self = this;
+
+    self.songs = [];
+
+    self.displaySongs = function() {
+      SongFetcherService.getAlbum('10v912xgTZbjAtYfyKWJCS')
+      .then(function(data) {
+        self.songs = data;
+      });
+    };
+
+    self.displaySongs();
+
+    function IntroGameController() {
+      this.introGame = "IntroGame1";
+    };
+
+  }]);
