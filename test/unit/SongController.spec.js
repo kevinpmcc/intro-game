27d9e0ef8fb4b4e320659ccba04731951918c732
Pete Burch <peteburch223@gmail>
2016-05-09 13:27:59 +0100

    TDD on song screen

--- test/unit/SongController.spec.js
+++ test/unit/SongController.spec.js
@@ -2,12 +2,22 @@ describe('SongController', function() {
   beforeEach(module('introGame.songController'));
 
   var SongFetcherService;
+  var ngAudio;
   var ctrl;
+  var sound;
 
   beforeEach(inject(function($rootScope, $controller) {
     SongFetcherService = jasmine.createSpyObj('SongFetcherService', ['currentSong']);
     SongFetcherService.currentSong.and.returnValue("song1");
+
+    sound = jasmine.createSpyObj('sound', ['play']);
+
+    ngAudio = jasmine.createSpyObj('ngAudio',['load']);
+    ngAudio.load.and.returnValue(sound);
+
     ctrl = $controller('SongController', {
+      ngAudio: ngAudio,
+      sound: sound,
       SongFetcherService: SongFetcherService,
     });
   }));
@@ -19,6 +29,15 @@ describe('SongController', function() {
 
   it('has default SONGLENGTH of 1', function () {
     expect(ctrl.SONGLENGTH).toEqual(1);
+  });
+
+  describe('#playCurrentSong', function(){
+
+    it('plays song', function(){
+      ctrl.playCurrentSong();
+      expect(ngAudio.load).toHaveBeenCalled()
+      expect(sound.play).toHaveBeenCalled();
+    });
   })
 
 })
