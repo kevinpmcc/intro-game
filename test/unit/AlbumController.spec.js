1ef25c6341918e7fa4e1e6836e13127fe1ac95f4
Chris Parker <cjp214@gmail.com>
2016-05-09 16:40:16 +0100

    refactored album controller

--- test/unit/AlbumController.spec.js
+++ test/unit/AlbumController.spec.js
@@ -2,27 +2,31 @@ describe('AlbumController', function() {
   beforeEach(module('introGame.albumController'));
 
   var SongFetcherService, stateMock;
+  var AlbumFetcherService;
   var ctrl;
   var albumID = "10v912xgTZbjAtYfyKWJCS"
 
   beforeEach(inject(function($rootScope, $controller, $q) {
    deferred = $q.defer();
+   AlbumFetcherService = jasmine.createSpyObj('AlbumFetcherService', ['getAlbums']);
    SongFetcherService = jasmine.createSpyObj('SongFetcherService', ['getAlbum', 'nextSong']);
    SongFetcherService.getAlbum.and.returnValue($q.when(""));
+   AlbumFetcherService.getAlbums.and.returnValue($q.when(["koala bear"]));
    stateMock = jasmine.createSpyObj('$state spy', ['go']);
    scope = $rootScope;
 
    ctrl = $controller('AlbumController', {
      SongFetcherService: SongFetcherService,
+     AlbumFetcherService: AlbumFetcherService,
      $state: stateMock
    });
-
  }));
 
   it('stores album data in an array', function() {
     expect(ctrl.albums).toBeDefined();
+    scope.$apply();
     ctrl.loadAlbums();
-    expect(ctrl.albums[0].title).toEqual('Highway to Hell');
+    expect(ctrl.albums[0]).toEqual('koala bear');
   });
 
   describe('#loadSongToGuess', function() {
@@ -31,12 +35,15 @@ describe('AlbumController', function() {
       scope.$apply();
     });
 
-    it('calls songFetcherService.getAlbum', function() {
-      expect(SongFetcherService.getAlbum).toHaveBeenCalled();
-    });
-
     it('calls ctrl.changeToSongState', function() {
       expect(stateMock.go).toHaveBeenCalledWith('song',{});
     })
+  });
+
+  describe('#loadAlbums', function() {
+    it('calls albumFetcherService.getAlbums', function() {
+      ctrl.loadAlbums();
+      expect(AlbumFetcherService.getAlbums).toHaveBeenCalled();
+    });
   });
 });
